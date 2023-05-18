f3b7e7a9b473a28e8956d12214757d8a185cd305
James Seekings <seekings.jem@gmail.com>
2023-05-18 17:51:55 +0100

    third commit

--- provision_app.sh
+++ provision_app.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade -y
 
 sudo apt-get install nginx -y
 
-sudo systemctl start nginx -y
+sudo systemctl start nginx
 
 # install application dependant Node packages
 
@@ -28,3 +28,34 @@ sudo npm install pm2 -g
 
 # node app.js
 
+new_file="
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name 192.168.10.100;
+
+        location / {
+                proxy_pass http://localhost:3000;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection 'upgrade';
+                proxy_set_header Host $host;
+                proxy_cache_bypass $http_upgrade;
+        }
+
+        location /posts {
+                proxy_pass http://localhost:3000/posts;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection 'upgrade';
+                proxy_set_header Host $host;
+                proxy_cache_bypass $http_upgrade;
+    }
+}
+"
+new_file > /etc/nginx/sites-available/default
